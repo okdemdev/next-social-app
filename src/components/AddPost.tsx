0c48b432b3ac25222b247927414a881672e2132b
damian dev <inamida16@gmail.com>
2024-07-06 12:28:49 +0300

    clerk users to data base using ngrok

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,6 +1,11 @@
+import prisma from '@/lib/client'
+import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
 
 const AddPost = () => {
+  const { userId } = auth()
+  console.log(userId)
+
   return (
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* AVATAR */}
@@ -14,8 +19,12 @@ const AddPost = () => {
       {/* POST */}
       <div className="flex-1">
         {/* TEXT INPUT */}
-        <div className="flex gap-4">
-          <textarea placeholder="what is on your mind?" className="bg-slate-100 rounded-lg flex-1 p-2"></textarea>
+        <form className="flex gap-4">
+          <textarea
+            placeholder="what is on your mind?"
+            className="bg-slate-100 rounded-lg flex-1 p-2"
+            name="desc"
+          ></textarea>
           <Image
             src="/emoji.png"
             alt="post-avatar"
@@ -23,7 +32,8 @@ const AddPost = () => {
             height={20}
             className="w-5 h-5 object-cover rounded-full cursor-pointer self-end"
           />
-        </div>
+          <button>Send</button>
+        </form>
         {/* POST OPTIONS */}
         <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
           <div className="flex items-center gap-2 cursor-pointer">
